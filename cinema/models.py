df6fe187b32d13efc01912fba19d0cf493c257b6
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-11-14 16:12:28 +0200

    test6

--- cinema/models.py
+++ cinema/models.py
@@ -47,11 +47,7 @@ class Movie(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(
-        Movie,
-        on_delete=models.CASCADE,
-        related_name="movies"
-    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movies")
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
 
     class Meta:
